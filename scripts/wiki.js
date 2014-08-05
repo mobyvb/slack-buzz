ba60e5f0dbef0ed150288f41de54bd12262cffa4
Maximillian von Briesen <mobyvb@gmail.com>
2014-08-05 18:29:07 -0400

    add wiki.js again

---
+++ scripts/wiki.js
@@ -0,0 +1,21 @@
+var wikipedia = require("wikipedia-js");
+var S = require('string');
+
+module.exports = function(robot) {
+
+  robot.respond(/(wikipedia|wiki)( me)? (.*)/i, function(msg) {
+    options = {query: msg.match[3], format: 'html', summaryOnly: true};
+    wikipedia.searchArticle(options, function(err, htmlWikiText) {
+      if(err) {
+        msg.send('unexpected error: %s', err);
+        return;
+      }
+      if(htmlWikiText) {
+        var text = S(htmlWikiText).stripTags().s;
+        var firstParagraph = text.split('\n')[0];
+        msg.send(firstParagraph);
+      }
+      else msg.send('no article found');
+    });
+  });
+};
