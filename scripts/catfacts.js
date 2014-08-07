e4d55d319329da204a65995d4cb650b0f2e98c54
Maximillian von Briesen <mobyvb@gmail.com>
2014-08-07 19:46:11 -0400

    add catfacts

---
+++ scripts/catfacts.js
@@ -0,0 +1,31 @@
+var http = require('http');
+
+module.exports = function(robot) {
+  robot.hear(/(\s|^)(cat|cats|kitten|kittens)(\s|$|\.|,)/i, function(msg){
+    getCatFact(function(fact) {
+      msg.send(fact);
+    });
+  });
+
+  robot.respond(/(?:catfact)/i, function(msg) {
+    getCatFact(function(fact) {
+      msg.send(fact);
+    });
+  });
+};
+
+function getCatFact(cb) {
+  var body = '';
+  http.get('http://catfacts-api.appspot.com/api/facts', function(res) {
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      body = JSON.parse(body);
+      var fact = 'Thank you for subscribing to catfacts!\n' + body.facts;
+      cb(fact);
+    });
+  }).on('error', function(e) {
+    cb('Got error: ' + e.message);
+  });
+}
