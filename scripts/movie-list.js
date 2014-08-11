c618ac46e6eb7b01ed618f4b0800c9a1e214a7b7
Maximillian von Briesen <mobyvb@gmail.com>
2014-08-10 21:41:41 -0400

    add remove, check, and uncheck functions

--- scripts/movie-list.js
+++ scripts/movie-list.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
 mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
@@ -14,24 +15,43 @@ var movieSchema = mongoose.Schema({
 var Movie = mongoose.model('Movie', movieSchema);
 
 module.exports = function(robot) {
-  robot.respond(/movie list/i, function(msg) {
+  robot.respond(/list movies/i, function(msg) {
     Movie.find(function(err, movies) {
       if(err) return msg.send(error);
-      msg.send('Movie List:\n' + movies);
+      var response = 'Movie List:\n';
+      movies.forEach(function(movie) {
+        response += movie.checked ? ':heavy_check_mark: ' : '';
+        response += movie.name + '\n';
+      });
+      msg.send(response);
     });
   });
   robot.respond(/(add) (movie) (.*)/i, function(msg) {
     var movieName = msg.match[3];
-    var newMovie = new Movie({ name: movieName, checked: false });
-    newMovie.save(function() {
-      msg.send('hello');
-      msg.send(newMovie);
+    Movie.create({ name: movieName, checked: false }, function (err, movie) {
+      if (err) return msg.send(err);
+      msg.send('Added ' + movie.name + ' to list.');
     });
   });
   robot.respond(/(remove|rm) (movie) (.*)/i, function(msg) {
-
+    var movieName = msg.match[3];
+    Movie.remove({ name: movieName }, function (err) {
+      if (err) return msg.send(err);
+      msg.send('Removed ' + movieName);
+    });
   });
   robot.respond(/(check) (movie) (.*)/i, function(msg) {
-
+    var movieName = msg.match[3];
+    Movie.update({ name: movieName }, { checked: true }, function(err) {
+      if (err) return msg.send(err);
+      msg.send('Checked ' + movieName + ' off list.');
+    });
+  });
+  robot.respond(/(uncheck) (movie) (.*)/i, function(msg) {
+    var movieName = msg.match[3];
+    Movie.update({ name: movieName }, { checked: false }, function(err) {
+      if (err) return msg.send(err);
+      msg.send('Unchecked ' + movieName + '.');
+    });
   });
 }
